7da5daf6daf8711c631f76e0efa219256078d95a
Tanabut Ngoensri <tangg6@outlook.com>
2022-01-29 22:33:19 +0700

    Edit Header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet,Image } from 'react-native'
 
 const Header = (props) => {
     return (
         <View style={styles.header}>
+            <Image 
+                style={styles.img}
+                source={require('../assets/moc_logo.png')}
+            />
             <Text style={styles.text}>
                 {props.name}
             </Text>
@@ -13,14 +17,20 @@ const Header = (props) => {
 
 const styles = StyleSheet.create({
     header: {
-        height: 60,
-        padding: 15,
+        height: 90,
+        paddingTop: 10,
         backgroundColor: '#0a214a'
     },
     text: {
         color: '#fff',
         fontSize: 24,
         textAlign: 'center'
+    },
+    img : {
+       
+        weight:70,
+        height:70,
+        resizeMode: "contain",
     }
 })
 
--- screens/ShowPricePage.js
+++ screens/ShowPricePage.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Header from '../components/Header'
 
 const ShowPricePage = () => {
   return (
     <SafeAreaView>
+        <Header name='MOC' />
         <Text>
-            
+            Hello MOC
         </Text>
     </SafeAreaView>
   );
