ec939899693d7cc743e0e2d90a74d2c2c95e986f
poom-cyber <poompim083@gmail.com>
2022-02-13 00:26:53 +0700

    ีร ียกฟะำ

--- src/screens/RecommendPage.js
+++ src/screens/RecommendPage.js
@@ -7,7 +7,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import Moc_logo from '../../assets/moc_logo.png';
 import firestore from '@react-native-firebase/firestore';
 import auth from "@react-native-firebase/auth"
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, StyleSheet } from 'react-native';
 
 
 const RecommendPage = ({ navigation }) => {
@@ -55,7 +55,6 @@ const RecommendPage = ({ navigation }) => {
                     marginBottom: '3%',
                     marginTop: '3%',
                     fontFamily:"Mitr-Light" 
-
                 }}>Recommend</Text>
 
             {Loading ? <ActivityIndicator /> : (
@@ -64,31 +63,35 @@ const RecommendPage = ({ navigation }) => {
                     data={favoriteArray}
                     renderItem={({ item }) => (
                         <ListItem
-                            Component={TouchableScale}
-                            friction={0} //
-                            tension={200} // These props are passed to the parent component (here TouchableScale)
-                            activeScale={0.95} //
-                            linearGradientProps={{
-                                colors: ['#1544E2', '#0A214A'],
-                                start: { x: 1, y: 0 },
-                                end: { x: 0.2, y: 0 },
-                            }}
-                            ViewComponent={LinearGradient}
-                            containerStyle={{
-                                marginHorizontal: 16,
-                                marginVertical: 8,
-                                borderRadius: 8,
-                            }}
-                            onPress={() =>
-                                navigation.navigate('ShowPricePage', { id: item.product_id })
-                            }
-                        >
-                            <Avatar source={Moc_logo} rounded />
-                            <ListItem.Content>
-                                <ListItem.Title style={{ fontSize: 22, color: '#FFC511', fontWeight: '600',fontFamily:"Mitr-Light" }}>{`${item.product_name}`}</ListItem.Title>
-                                <ListItem.Subtitle style={{ color: '#CED0CE', fontFamily:"Mitr-Light" }}>{item.product_id}</ListItem.Subtitle>
-                            </ListItem.Content>
-                        </ListItem>
+                        Component={TouchableScale}
+                        friction={0} //
+                        tension={200} // These props are passed to the parent component (here TouchableScale)
+                        activeScale={0.95} //
+                        linearGradientProps={{
+                          colors: ['#1544E2', '#0A214A'],
+                          start: { x: 1, y: 0 },
+                          end: { x: 0.2, y: 0 },
+                        }}
+                        ViewComponent={LinearGradient}
+                        containerStyle={{
+                          marginHorizontal: 4,
+                          marginVertical: 4,
+                          borderRadius: 8,
+                        }}
+                        onPress={() =>
+                          navigation.navigate('ShowPricePage', { id: item.product_id })
+                        }
+                      >
+                        <Avatar source={Moc_logo} rounded />
+                        <ListItem.Content>
+                          <ListItem.Title style={{ fontSize: 22, color: '#FFC511', fontWeight: '700', fontFamily: "Mitr-Light" }}>{`${item.product_name}`}</ListItem.Title>
+                          <View style={styles.TextContainer1}>
+                            <ListItem.Subtitle style={{ color: '#CED0CE', fontFamily: "Mitr-Light" }}>{item.group_name} </ListItem.Subtitle>
+                            <ListItem.Subtitle style={{ color: '#CED0CE', fontFamily: "Mitr-Light" }}>{item.categoty_name} </ListItem.Subtitle>
+                            <ListItem.Subtitle style={{ color: '#CED0CE', fontFamily: "Mitr-Light" }}>รหัสสินค้า : {item.product_id}</ListItem.Subtitle>
+                          </View>
+                        </ListItem.Content>
+                      </ListItem>
                     )}
                     keyExtractor={item => item.product_id}
                 />
@@ -96,4 +99,37 @@ const RecommendPage = ({ navigation }) => {
             </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#393E46',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+      paddingTop: 10,
+    },
+    TextContainer1: {
+      flexDirection: 'row',
+      marginBottom: '1%',
+    },
+    loginButtonText: {
+      textAlign: 'center',
+      color: '#F0FFFF',
+      fontWeight: 'bold',
+      fontSize: 20,
+      padding: 15
+    },
+    logoutButton: {
+      marginVertical: 10,
+      backgroundColor: '#b53531',
+      width: 320,
+      height: 60,
+      borderRadius: 10,
+      shadowColor: "#000000",
+      shadowOpacity: 5,
+      shadowRadius: 5,
+      elevation: 5
+    },
+  });
+
+
 export default (RecommendPage);
