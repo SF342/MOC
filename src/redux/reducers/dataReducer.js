7cd6c6c026e80a1700b7611619be2bca8658197b
poom-cyber <poompim083@gmail.com>
2022-04-21 23:57:54 +0700

    Add photo v2

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -27,7 +27,7 @@ const initialState = {
       url: 'https://firebasestorage.googleapis.com/v0/b/sf342-8685d.appspot.com/o/AnimalIcon%2Fpig%20(1).png?alt=media',
     },
     {
-      name: 'วัว',
+      name: 'เนื้อโค',
       url: 'https://firebasestorage.googleapis.com/v0/b/sf342-8685d.appspot.com/o/AnimalIcon%2Fcow%20(1).png?alt=media',
     },
     {
@@ -47,8 +47,32 @@ const initialState = {
       url : "https://www.smeleader.com/wp-content/uploads/2019/08/%E0%B8%A3%E0%B8%A7%E0%B8%A1%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B9%82%E0%B8%A3%E0%B8%87%E0%B8%87%E0%B8%B2%E0%B8%99%E0%B8%9C%E0%B8%A5%E0%B8%B4%E0%B8%95%E0%B9%80%E0%B8%AA%E0%B9%89%E0%B8%99%E0%B8%81%E0%B9%8B%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%95%E0%B8%B5%E0%B9%8B%E0%B8%A2%E0%B8%A7-%E0%B9%80%E0%B8%AA%E0%B9%89%E0%B8%99%E0%B8%82%E0%B8%99%E0%B8%A1%E0%B8%88%E0%B8%B5%E0%B8%99-%E0%B9%80%E0%B8%AA%E0%B9%89%E0%B8%99%E0%B8%9A%E0%B8%B0%E0%B8%AB%E0%B8%A1%E0%B8%B5%E0%B9%88.jpg"
     },
     {
+      name : "ผักบุ้งจีน",
+      url : "https://file.sogoodweb.com/upload/156/RCyj3QwIUY.png"
+    },
+    {
       name : "ผักคะน้า",
-      url : "https://e7.pngegg.com/pngimages/257/516/png-clipart-chinese-broccoli-spring-greens-vegetable-kale-spinach-the-basket-of-kale-leaf-vegetable-food-thumbnail.png"
+      url : "https://firebasestorage.googleapis.com/v0/b/sf342-8685d.appspot.com/o/AnimalIcon%2Fpngegg%20(5).png?alt=media&token=a1dde6dd-1850-41b7-aceb-ee969de3124d"
+    },
+    {
+      name : "ผักกวางตุ้ง",
+      url : "https://firebasestorage.googleapis.com/v0/b/sf342-8685d.appspot.com/o/AnimalIcon%2Fkisspng-chinese-cuisine-choy-sum-cantonese-cuisine-vegetab-cabbage-material-5a81f95e25c691.8514624715184674221547.png?alt=media&token=c4b4d61f-8c9c-4164-aff5-2e905e73d81b"
+    },
+    {
+      name: "ผักกาด",
+      url : "https://firebasestorage.googleapis.com/v0/b/sf342-8685d.appspot.com/o/AnimalIcon%2Fpngegg%20(6).png?alt=media&token=1c2b37c4-2e66-481e-85d8-8b109f286970"
+    },
+    {
+      name : "กะหล่ำ",
+      url : "https://firebasestorage.googleapis.com/v0/b/sf342-8685d.appspot.com/o/AnimalIcon%2Fpngegg%20(7).png?alt=media&token=88460f5a-04db-40ff-9a99-3393ba429f02"
+    },
+    {
+      name : "มะระจีน",
+      url : "https://adeq.or.th/wp-content/uploads/2019/04/%E0%B8%A1%E0%B8%B0%E0%B8%A3%E0%B8%B0%E0%B8%88%E0%B8%B5%E0%B8%9901.jpg"
+    },
+    {
+      name : "มะเขือเทศ",
+      url : "https://firebasestorage.googleapis.com/v0/b/sf342-8685d.appspot.com/o/AnimalIcon%2Fkisspng-tomato-juice-clip-art-vegetable-album-5a9ac81bd7a464.1927236615200932118833.png?alt=media&token=c69bb2ad-2355-4cb6-8173-e3274613ce9d"
     }
   ],
 };
